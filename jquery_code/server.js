d6217d1403ae595918273caf51f602e937b525c5
ReakingAd <ltvieri@163.com>
2017-02-20 23:54:26 +0800

    02202354

--- jquery_code/server.js
+++ jquery_code/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer( (req,res) => {
-    // res.writeHead(200,{'Access-Control-Allow-Origin':'*'});
-    res.writeHead(200);
+    res.writeHead(200,{'Access-Control-Allow-Origin':'*'});
+    // res.writeHead(200);
     res.end('kkk');
 }).listen(1999);
--- jquery_code/test.js
+++ jquery_code/test.js
@@ -1,25 +1,20 @@
-;(function(global, factory) {
-    factory(global);
-}(typeof window !== "undefined" ? window : this, function(window, noGlobal) {
-    var jQuery = function( selector, context ) {
-		return new jQuery.fn.init( selector, context );
-	};
-	jQuery.fn = jQuery.prototype = {};
-	// 核心方法
-	// 回调系统
-	// 异步队列
-	// 数据缓存
-	// 队列操作
-	// 选择器引
-	// 属性操作
-	// 节点遍历
-	// 文档处理
-	// 样式操作
-	// 属性操作
-	// 事件体系
-	// AJAX交互
-	// 动画引擎
-	return jQuery;
-}));
+let Callbacks = {
+	callback:[],
+	add:function( fn ){
+		this.callback.push(fn);
+	},
+	fire:function(){
+		this.callback.forEach( (item,index) => {
+			item();
+		})
+	}
+}
 
-console.log( global.jQuery );
+Callbacks.add(function(){
+	console.log(111111);
+});
+Callbacks.add(function(){
+	console.log(22222222222);
+});
+
+Callbacks.fire();
