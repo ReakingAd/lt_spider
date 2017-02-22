70421be3419ca84890109a6a540008b9f50591e0
litao <litao@waixie.com>
2017-02-22 18:24:27 +0800

    add pubsub folder

--- pubsub/main.js
+++ pubsub/main.js
@@ -1,22 +1,42 @@
-let salesOffices = {};
-
-salesOffices.clientList = [];
-salesOffices.listen = function( fn ){
-    this.clientList.push( fn );
-}
-salesOffices.trigger = function(){
-    for( var i=0,fn;fn=this.clientList[i];i++ ){
-        fn.apply( this,arguments );
+let event = {
+    client:{},
+    listen:function(event,cb){
+        if( !this.client[ event ] ){
+            this.client[ event ] = [];
+        }
+        this.client[ event ].push(cb);
+    },
+    trigger:function(event,info){
+        if( !this.client[ event ] || this.client[ event ].length === 0 ){
+            return false;
+        }
+        this.client[ event ].forEach( (item,index) => {
+            item(info);
+        });
+    },
+    remove:function(event){a
+        if( !client[ event ] ){
+            return false;
+        }
+        client[ event ] = undefined;
     }
 }
 
-salesOffices.listen( function(price,squareMeter){
-    console.log('价格= ' + price);
-    console.log( 'squareMeter= ' + squareMeter );
-});
-salesOffices.listen( function(price,squareMeter){
-    console.log('价格= ' + price);
-    console.log( 'squareMeter= ' + squareMete );
-});
+function installEvent(obj){
+    for( key in event ){
+        obj[ key ] = event[ key ];
+    }
+}
 
-salesOffices.trigger(20000,88);
+// event.listen('80p',function(info){
+//     console.log('I got the info: ' + info);
+// });
+// event.trigger('80p','总价800000');
+
+let salesOffice = {};
+
+installEvent(salesOffice);
+salesOffice.listen('90p',info => {
+    console.log('I got the info: ' + info);
+});
+salesOffice.trigger('90p','总价=900000');
---
+++ pubsub/pubsub.js
@@ -0,0 +1,67 @@
+/*
+@authoer Litao
+@desc   观察者模式的简单实现。可以订阅、发布、取消订阅
+@usage
+    <script src="pubsub.js"></script>
+    <script>
+        var myCallback = function(data){
+            console.log( data );
+        }
+        Event.listen('event1',myCallback);
+        Event.trigger('event1','sdfsdfsdf');
+        Event.remove('event1',myCallback);
+        Event.trigger('event1','sdfsdfsdf');
+    </script>
+@ 问题1： 如果需要remove()取消订阅，则在listen()订阅的时候就不许穿入具名函数。
+@ 问题2： 如果不同人使用，为同一个事件绑定2个回调，不幸函数名重复，则在remove()的时候，会把两个回调都取消订阅。
+*/
+var Event = (function(){
+    var eContainer = {};
+    var listen = function( event,cb ){
+        if( !eContainer[ event ] ){
+            eContainer[ event ] = [];
+        }
+        if( typeof cb !== 'function' ){
+            throw Error('param error,\'function\' needed');
+        }
+        eContainer[ event ].push(cb);
+    }
+    var trigger = function( event,data ){
+        if( !eContainer[ event ] ){
+            console.warn( 'no event registered named ' + event );
+            return false;
+        }
+        else if( eContainer[ event ].length === 0 ){
+            console.warn( 'no callback registered for event ' + event );
+        }
+        eContainer[ event ].forEach(function(item,index){
+            item(data);
+        });
+    }
+    /*
+    @要求remove()在使用时，cb必须是一个具名函数，且在listen()的时候，穿入的也是同一个具名函数。
+    */
+    var remove = function( event,cb ){
+        var cbs = eContainer[ event ];
+
+        if( !cbs ){
+            return false;
+        }
+        // 如果未传入回调cb，则删除当前穿入的event事件。
+        if( !cb ){
+            delete eContainer[ event ];
+        }
+        else{
+            for( var i=0;i<cbs.length;i++ ){
+                if( cb === cbs[i] ){
+                    cbs.splice(i,1);
+                }
+            }
+        }
+    }
+    return {
+        listen:listen,
+        trigger:trigger,
+        remove:remove
+    }
+})();
